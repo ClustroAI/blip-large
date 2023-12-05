2f7b2af7dec69f62599313e9e676ce218dad0403
Yingqiang Liu <53621227+Remilistrasza@users.noreply.github.com>
2023-12-04 19:41:52 -0700

    Add model version

--- model_invoke.py
+++ model_invoke.py
@@ -3,8 +3,14 @@ from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 import json
 
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large").to("cuda")
+processor = BlipProcessor.from_pretrained(
+    "Salesforce/blip-image-captioning-large",
+    revision="e33defe055114d86bec9c0705b71e590cfc0694c"
+)
+model = BlipForConditionalGeneration.from_pretrained(
+    "Salesforce/blip-image-captioning-large",
+    revision="e33defe055114d86bec9c0705b71e590cfc0694c"
+).to("cuda")
 
 def invoke(input_text):
     try:
